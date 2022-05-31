838c54d18f2446e7ae6f4ffd3f207b0b598d5be6
Maksym Kaczorowski <maksym73@gmail.com>
2022-05-31 18:00:19 +0200

    Adding the same background color to all rows and adding selection by id option

--- product_list_app/src/components/Data.js
+++ product_list_app/src/components/Data.js
@@ -1,14 +1,15 @@
 import React, {useEffect, useState} from 'react'
 import {DataGrid} from '@material-ui/data-grid'
+import Box from '@mui/material/Box';
 
 
 const columns = [
-    {field: 'id', headerName: 'ID'},
-    {field: 'name', headerName: 'Name',  width: 300},
-    {field: 'year', headerName: 'Year', width: 600}
+    {field: 'id', headerName: 'ID', cellClassName: 'super-app-theme--cell'},
+    {field: 'name', headerName: 'Name', cellClassName: 'super-app-theme--cell',  width: 300},
+    {field: 'year', headerName: 'Year', cellClassName: 'super-app-theme--cell', width: 600},
 ]
 const DataTable = () =>{
-
+    const [select, setSelection] = useState([]);
     const [ tableData, setTableData] = useState([])
     useEffect(() =>{
         fetch("https://reqres.in/api/products")
@@ -17,14 +18,30 @@ const DataTable = () =>{
     })
 
     return(
-        <div style={{height: 500, width: '100%' }}>
+
+        <Box
+
+                sx={{
+                    height: 500,
+                    width: '100%',
+                    '& .super-app-theme--cell': {
+                        backgroundColor: '#98B2D1',
+                        color: '#1a3e72',
+                        fontWeight: '600',
+                    },
+                }}
+            >
              <DataGrid
              rows={tableData}
              columns={columns}
              pageSize={5}
-             checkboxSelection
+             onSelectionChange={(newSelection) => {
+                 setSelection(newSelection.rows)
+             }}
              />
-        </div>
+            <h1>{select.map((data) => data.data.id)}</h1>
+        </Box>
+
     )
 }
 export { DataTable };
